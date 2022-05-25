24d1583865735606dadbc0440d7e614af9b2beec
emilpeters <emilp19@ru.is>
2022-05-25 16:47:38 +0000

    add event info html

--- user/views.py
+++ user/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from user.models import Account
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'user/index.html')
+    return render(request, 'user/index.html',{
+        'user': Account.objects.all()
+    })
