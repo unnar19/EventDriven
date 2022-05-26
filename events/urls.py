7826e25822c64f545d291ff03af502390d5c0c25
emilpeters <emilp19@ru.is>
2022-05-26 11:39:08 +0000

    fuck off

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #http://localhost:8000/events
     path('', views.index, name='event-index'),
+    path('int:id', views.get_event_by_id, name ='event_details')
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from events.models import Event
 
 # Create your views here.
 def index(request):
     events = {'events': Event.objects.all().order_by('name')}
     return render(request, 'events/index.html', events)
+
+def get_event_by_id(request, id):
+    return render(request,'events/event_dietails.html', {
+        'event': get_object_or_404(Event, pk=id)
+    })
+
+# def get_catagory_of_event(request, id):
+#     return render(request,'events/event_dietails.html', {
+#         'event': get_object_or_404(Categories, pk=id)
+#     })
