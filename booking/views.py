48a186707c56792c449a3896c1821ce03a9d0bbc
unnar19 <unnar19@ru.is>
2022-05-30 10:06:32 +0000

    add booking/id view

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from events.models import Event,Ticket
 
 def index(request):
     return render(request, 'booking/index.html')
 
+def book_an_event(request, id):
+    return render(request, 'booking/booking_details.html', {
+        'event': get_object_or_404(Event, pk=id),
+        'price': get_list_or_404(Ticket, event_id=id)[0].price
+    })
