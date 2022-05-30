36ebda30d8f75e8a4ac780a7b2a23df1dc06fa83
emilpeters <emilp19@ru.is>
2022-05-30 12:41:04 +0000

    login stuff

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from events.models import Event,Ticket
@@ -6,7 +7,7 @@ from events.models import Event,Ticket
 def index(request):
     return render(request, 'booking/index.html')
 
-
+@login_required
 def book_an_event(request, id):
     return render(request, 'booking/booking_details.html', {
         'event': get_object_or_404(Event, pk=id),
