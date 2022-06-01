9aab2e1f65f42ca8fce91c2f91c869ad8bf4b36c
unnar19 <unnar19@ru.is>
2022-06-01 21:31:48 +0000

    alot of stuff

--- booking/forms/booking_form.py
+++ booking/forms/booking_form.py
@@ -29,11 +29,25 @@ class CreatePaymentForm(ModelForm):
     class Meta:
         model = Payment
         exclude = ['id']
-        fields = ('name_of_card_holder','card_number','exp_date','cvc')
+        # fields = ('name_of_card_holder','card_number','exp_date','cvc')
         widgets = {
-            'name_of_card_holder': widgets.TextInput(attrs={'class': 'form-control'}),
-            'card_number': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'exp_date': widgets.DateInput(attrs={'class': 'form-control'}),
-            'cvc': widgets.NumberInput(attrs={'class': 'form-control'})
+            'subtotal': widgets.TextInput(attrs={'class': 'form-control'}),
 
+        }
+
+class BookEventForm(ModelForm):
+    class Meta:
+        model = Booking
+        exclude = ['id','user_id','event_id']
+        labels = {
+             'amount':""
+             }
+        widgets = {
+            'amount': widgets.NumberInput(attrs={'class': 'hiddenfield','id': 'form-amount','value':'1'}),
+            'full_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'street_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'house_num': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'zip': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'country': widgets.TextInput(attrs={'class': 'form-control'}),
+            'city': widgets.TextInput(attrs={'class': 'form-control'})
         }
