f611ea5081a453bf5db440984d12adb9fc50e967
unnar19 <unnar19@ru.is>
2022-06-01 11:17:49 +0000

    make some model fields nullable

--- booking/models.py
+++ booking/models.py
@@ -2,23 +2,23 @@ from django.db import models
 from user.models import Account
 from events.models import Event
 
-#'name_of_card_holder','card_number','exp_date','cvc'
-
-class Payment(models.Model):
-    time = models.TimeField()
-    date = models.DateField()
-    subtotal = models.IntegerField()
-    name_of_card_holder = models.CharField(max_length=200)
-    card_number = models.CharField(max_length=16)
-    exp_date = models.DateField()
-    cvc = models.IntegerField()
 
 class Booking(models.Model):
-    email = models.ForeignKey(Account, on_delete=models.CASCADE)
-    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    email = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    event_id = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
     amount = models.IntegerField()
-    address = models.CharField(max_length=150)
-    zip = models.IntegerField()
-    country = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    address = models.CharField(max_length=150, blank=True, null=True)
+    zip = models.IntegerField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+
+
+class Payment(models.Model):
+    time = models.TimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
+    subtotal = models.IntegerField()
+    booking_id = models.ForeignKey(Booking, on_delete=models.SET_NULL, null=True)
+    name_of_card_holder = models.CharField(max_length=250)
+    card_number = models.IntegerField()
+    exp_date = models.DateField()
+    cvc = models.IntegerField()
--- events/models.py
+++ events/models.py
@@ -12,7 +12,7 @@ class Event(models.Model):
     image_url = models.CharField(max_length=9999)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
     time = models.TimeField()
     start_date = models.DateField()
     end_date = models.DateField()
