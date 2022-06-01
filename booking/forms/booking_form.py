56520bc5b516a48e384dc8e379dce7ab7baec41e
unnar19 <unnar19@ru.is>
2022-06-01 15:26:11 +0000

    delivery autofill

--- booking/forms/booking_form.py
+++ booking/forms/booking_form.py
@@ -7,12 +7,20 @@ class CreateBookingForm(ModelForm):
     hidden_del = forms.BooleanField(widget=forms.HiddenInput, initial=True)
     class Meta:
         model = Booking
-        exclude = ['payment_id', 'id', 'ticket_id','event_id']
+        exclude = ['id']
+        # labels = {
+        #     'email':"",
+        #     'event_id':"",
+        #     'amount':""
+        #     }
         widgets = {
-            'email': widgets.TextInput(attrs={'class': 'form-control'}),
-            'amount': widgets.TextInput(attrs={'class': 'form-control'}),
-            'address': widgets.TextInput(attrs={'class': 'form-control'}),
-            'zip': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.TextInput(attrs={'class': 'hiddenfield9','id': 'form-email'}),
+            'event_id': widgets.NumberInput(attrs={'class': 'hiddenfield9','id': 'form-event_id'}),
+            'amount': widgets.NumberInput(attrs={'class': 'hiddenfield9','id': 'form-amount','value':'1'}),
+            'full_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'street_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'house_num': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'zip': widgets.NumberInput(attrs={'class': 'form-control'}),
             'country': widgets.TextInput(attrs={'class': 'form-control'}),
             'city': widgets.TextInput(attrs={'class': 'form-control'})
         }
@@ -26,7 +34,7 @@ class CreatePaymentForm(ModelForm):
         fields = ('name_of_card_holder','card_number','exp_date','cvc')
         widgets = {
             'name_of_card_holder': widgets.TextInput(attrs={'class': 'form-control'}),
-            'card_number': widgets.TextInput(attrs={'class': 'form-control'}),
+            'card_number': widgets.NumberInput(attrs={'class': 'form-control'}),
             'exp_date': widgets.DateInput(attrs={'class': 'form-control'}),
-            'cvc': widgets.TextInput(attrs={'class': 'form-control'})
+            'cvc': widgets.NumberInput(attrs={'class': 'form-control'})
         }
