54032b005b2e7fb32298b12a6076db0ac8813b28
unnar19 <unnar19@ru.is>
2022-06-01 10:24:00 +0000

    combine formdel and formpay

--- booking/forms/booking_form.py
+++ booking/forms/booking_form.py
@@ -3,7 +3,23 @@ from django import forms
 from booking.models import Booking, Payment
 
 
+class CreateBookingForm(ModelForm):
+    hidden_del = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    class Meta:
+        model = Booking
+        exclude = ['payment_id', 'id', 'ticket_id','event_id']
+        widgets = {
+            'email': widgets.TextInput(attrs={'class': 'form-control'}),
+            'amount': widgets.TextInput(attrs={'class': 'form-control'}),
+            'address': widgets.TextInput(attrs={'class': 'form-control'}),
+            'zip': widgets.TextInput(attrs={'class': 'form-control'}),
+            'country': widgets.TextInput(attrs={'class': 'form-control'}),
+            'city': widgets.TextInput(attrs={'class': 'form-control'})
+        }
+
+
 class CreatePaymentForm(ModelForm):
+    hidden_pay = forms.BooleanField(widget=forms.HiddenInput, initial=True)
     class Meta:
         model = Payment
         exclude = ['id']
@@ -13,17 +29,4 @@ class CreatePaymentForm(ModelForm):
             'card_number': widgets.TextInput(attrs={'class': 'form-control'}),
             'exp_date': widgets.DateInput(attrs={'class': 'form-control'}),
             'cvc': widgets.TextInput(attrs={'class': 'form-control'})
-        }
-
-class CreateBookingForm(ModelForm):
-    class Meta:
-        model = Booking
-        exclude = ['payment_id', 'id', 'ticket_id']
-        widgets = {
-            'email': widgets.TextInput(attrs={'class': 'form-control'}),
-            'amount': widgets.TextInput(attrs={'class': 'form-control'}),
-            'address': widgets.TextInput(attrs={'class': 'form-control'}),
-            'zip': widgets.TextInput(attrs={'class': 'form-control'}),
-            'country': widgets.TextInput(attrs={'class': 'form-control'}),
-            'city': widgets.TextInput(attrs={'class': 'form-control'})
         }
